776c17b8a7486fbf28bfaf2a3c8251f3f0d3df22
Ben <bchadwick94@gmail.com>
2018-10-11 09:39:54 +0100

    remove password field

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20181010223309) do
+ActiveRecord::Schema.define(version: 20181011083851) do
 
   create_table "answers", force: :cascade do |t|
     t.string "text"
@@ -33,7 +33,6 @@ ActiveRecord::Schema.define(version: 20181010223309) do
     t.string "email"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
-    t.string "password"
     t.string "password_confirmation"
     t.string "password_digest"
   end
