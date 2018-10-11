8f2aaaafc57b14a258e2909d68d5f9a1120d14cd
Ben <bchadwick94@gmail.com>
2018-10-11 08:31:10 +0100

    custom error pages

--- spec/controllers/user_controller_spec.rb
+++ spec/controllers/user_controller_spec.rb
@@ -2,6 +2,7 @@ require "rails_helper"
 
 RSpec.describe UsersController do
   let(:user) { User.create(name: 'test', email: 'test@email.ie', password: 'test123', password_confirmation: 'test123') }
+  let(:question) { Question.create(title: 'Where for lunch?', text: 'pick a restraunt', user: user) }
 
   describe "GET index" do
     it "assigns @users" do
@@ -25,11 +26,20 @@ RSpec.describe UsersController do
     end
 
     context "when valid fields" do
-      it "creates" do 
+      it "creates a user and redirect to show" do 
         post :create, params: { user: { name: 'name', email: 'some@test.ie'} }
 
         expect(assigns(:user).name).to eq('name')
       end
+    end
+  end
+
+  describe "GET show" do
+    it "shows the user and their questions" do 
+      get :show, params: {id: user.id}
+
+      expect(response).to render_template(:show)
+      expect(assigns(:questions)).to eq([question])
     end
   end
 end
